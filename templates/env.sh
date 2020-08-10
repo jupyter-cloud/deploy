b8b285842cedfbdf5868a2b352deca8bc147a2e3
Ken Pu <kenpuca@gmail.com>
2020-08-10 12:40:13 -0400

    creation

---
+++ templates/env.sh
@@ -0,0 +1,7 @@
+export COURSE=math101
+export PORT=9876
+export LOCAL_NOTEBOOK_IMAGE=jupytercloud/minimal-notebook
+export HOST_DATA_VOLUME=${PWD}/data
+export NB_UID=`id -u`
+export NB_GID=`id -g`
+
